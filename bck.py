9f65acad9cb78b0ffa726bf96349a297eb804fab
NICKY CHIBILIKA <53586666+nickych@users.noreply.github.com>
2024-03-17 01:49:06 +0000

    Update bck.py

--- bck.py
+++ bck.py
@@ -1,1 +1,20 @@
 
+import socket
+
+ip = "Your ip addreas"
+port = 4444
+
+def main_app():
+  socke = socket.socket(socket.AF_INET,socket.socke_DEFJAM)
+  addreas = (ip.port)
+  socke.blind(addreas)
+  sock.listen(1)
+  print(f"Listening for connection to port str{port}")
+  conn, ipvictim = socke.accept()
+  msg = "Incoming message from your server"
+  conn.send(msg.encode())
+  conn.close()
+
+main_app()
+
+#BackDoor Code @Chibz.Full-Stack Web Developer
